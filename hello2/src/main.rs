59949b2cb07c2c40a52ea0942f77eaf58d641fe2
Kayi Shum <kshum@linkedin.com>
2022-11-24 20:31:15 -0800

    [match] arguments

--- hello2/src/main.rs
+++ hello2/src/main.rs
@@ -1,8 +1,31 @@
 #[path = "./modules/try_syntax.rs"]
-
 mod try_syntax;
 use try_syntax::try_syntax;
 
+use std::env;
+use std::fs;
+
 fn main() {
-    try_syntax();
+    let arg_value = match env::args().nth(1) {
+        Some(input) => input,
+        None => panic!("Try some existing options"),
+    };
+
+    match arg_value.as_str() {
+        "try-syntax" => {
+            println!("Executing try_syntax()");
+            try_syntax()
+        },
+        &_ => {
+            println!("Executing words counter of given file name");
+
+            let file_contents = match fs::read_to_string(arg_value) {
+                Ok(contents) => contents,
+                Err(err) => panic!("{}", err),
+            };
+
+            let count_of_words = file_contents.split_whitespace().count();
+            println!("Number of words: {}", count_of_words)
+        }
+    }
 }
